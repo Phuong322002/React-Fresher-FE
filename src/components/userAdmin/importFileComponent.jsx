ffc9a601e9fc2b4f92c8446144dc0ea1a8b19a0c
PhuongVu <puong334@gmail.com>
2024-07-29 19:22:05 +0700

    importFileUSer & read file excel

--- src/components/userAdmin/importFileComponent.jsx
+++ src/components/userAdmin/importFileComponent.jsx
@@ -3,36 +3,88 @@ import { Button, Modal } from 'antd';
 import { useState } from 'react';
 import { CloudDownloadOutlined, InboxOutlined } from '@ant-design/icons';
 import { Upload } from 'antd';
+import * as XLSX from 'xlsx/xlsx';
 
-const { Dragger } = Upload;
-const dummyRequest = async ({ file, onSuccess }) => {
-    setTimeout(() => {
-        onSuccess("ok");
-    }, 1000);
-}
 
-const propsUpload = {
-    name: 'file',
-    multiple: true,
-    accept: ".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel",
-    customRequest: dummyRequest,
-    // action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-    onChange(info) {
-        const { status } = info.file;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
-    },
-    onDrop(e) {
-        console.log('Dropped files', e.dataTransfer.files);
-    },
-};
+
 const ImportFileUser = () => {
+
+
+    const { Dragger } = Upload;
+    const [importFileUser, setImportFileUser] = useState([])
+
+    const dummyRequest = async ({ file, onSuccess }) => {
+
+        setTimeout(() => {
+            onSuccess("ok");
+        }, 1000);
+    }
+
+
+
+    const propsUpload = {
+        name: 'file',
+        multiple: false,
+        maxCount: 1,
+        accept: ".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel",
+        customRequest: dummyRequest,
+        // action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+        onChange(info) {
+            const { status } = info.file;
+            if (status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (status === 'done') {
+                console.log('info.file:', info.fileList)
+                if (info.fileList && info.fileList.length > 0) {
+                    const file = info.fileList[0].originFileObj;
+                    console.log('FileUser: ', file)
+                    let reader = new FileReader();
+                    reader.readAsArrayBuffer(file);
+                    reader.onload = function (e) {
+                        console.log('e.target.result:', e.target.result, reader.result)
+
+                        let data = new Uint8Array(e.target.result);
+                        console.log('data:', data)
+                        let workbook = XLSX.read(data, { type: 'array' });
+                        console.log('workbook:', workbook)
+
+                        // find the name of your sheet in the workbook first
+                        let worksheet = workbook.Sheets[`${workbook.SheetNames[0]}`];
+                        // convert to json format
+                        const jsonData = XLSX.utils.sheet_to_json(worksheet);
+                        if (jsonData.length > 0) {
+
+                            const newArrUser = jsonData.map((user, index) => {
+                                return {
+                                    //this is a row
+                                    key: index,
+                                    fullName: user['Tên hiển thị'],
+                                    email: user['Email'],
+                                    phone: user['Số điện thoại']
+                                }
+                            })
+                            setImportFileUser(newArrUser)
+                        }
+                    };
+                    // reader.readAsArrayBuffer(file);
+                    //
+                }
+
+                message.success(`${info.file.name} file uploaded successfully.`);
+            } else if (status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+        onDrop(e) {
+            console.log('Dropped files', e.dataTransfer.files);
+        },
+    };
+
+
+
+
+    console.log('importFileUser:', importFileUser)
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const showModal = () => {
@@ -46,24 +98,20 @@ const ImportFileUser = () => {
     };
     const columns = [
         {
-            title: 'Name',
-            dataIndex: 'name',
+            title: 'Tên hiển thị ',
+            dataIndex: 'fullName',
 
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
+            title: 'Email',
+            dataIndex: 'email',
 
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
+            title: 'Phone',
+            dataIndex: 'phone',
 
         },
-    ];
-
-    const data = [
-
     ];
     const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
@@ -73,8 +121,8 @@ const ImportFileUser = () => {
             <Button onClick={showModal} style={{ background: "orange", borderColor: "yellow" }}>
                 <CloudDownloadOutlined /> Import File
             </Button>
-            <Modal maskClosable={false} title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <Dragger {...propsUpload}>
+            <Modal width='50%' maskClosable={false} title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <Dragger {...propsUpload} style={{ maxHeight: "180px" }}>
                     <p className="ant-upload-drag-icon">
                         <InboxOutlined />
                     </p>
@@ -85,7 +133,7 @@ const ImportFileUser = () => {
                     </p>
 
                 </Dragger>
-                <div style={{ border: '1px dashed', borderRadius: '5px', marginTop: '5px' }}>
+                <div style={{ border: '1px dashed', borderRadius: '5px', marginTop: '12px' }}>
                     <Table
                         title={() => {
                             return (
@@ -95,13 +143,13 @@ const ImportFileUser = () => {
                             )
                         }}
                         columns={columns}
-                        dataSource={data}
+                        dataSource={importFileUser}
                         onChange={onChange}
                         pagination={false}
                     />
                 </div>
 
-            </Modal>
+            </Modal >
         </>
 
     )
