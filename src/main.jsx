b8924751bb3f70a85b7f6ef04af50dc3d888f0aa
PhuongVu <puong334@gmail.com>
2024-08-20 11:21:21 +0700

    updatevercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
